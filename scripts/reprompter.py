c25dfb888fc617002dfa00286d54fe0de2a7a46c
ogoun <ogoun.er@yandex.ru>
2025-01-25 21:53:48 +0300

    Update reprompter.py
    1. Options for context and text improvements have been moved from settings to a tab.
    2. The Use Context option now works.
    3. Refactoring

--- scripts/reprompter.py
+++ scripts/reprompter.py
@@ -3,29 +3,76 @@ import contextlib
 import gradio as gr
 
 from openai import OpenAI
-
 from modules import scripts, script_callbacks, shared
 from modules.shared import opts, state
 
 REPROMPTER = "Remprompter"
+BASE_DIR = scripts.basedir()
 
-sys_prompt_file_path = os.path.join(scripts.basedir(), 'sys_prompt.txt')
-with open(sys_prompt_file_path, 'r', encoding='UTF-8') as sys_prompt_file:
-    sys_prompt = sys_prompt_file.read()
+# OpenAPI connector
+class LLMOpenAIProvider():
+    def __init__(self, host, model, key):
+        self.host = host
+        self.model = model
+        self.key = key
 
-translate_sys_prompt_file_path = os.path.join(scripts.basedir(), 'translate_sys_prompt.txt')
-with open(translate_sys_prompt_file_path, 'r', encoding='UTF-8') as translate_sys_prompt_file:
-    translate_sys_prompt = translate_sys_prompt_file.read()
+    def call_llm(self, content):
+        client = OpenAI( base_url = self.host, api_key=self.key, )
+        response = client.chat.completions.create(model=self.model, messages=content)
+        return response.choices[0].message.content
+
+class PromptBuilder():
+
+    def __init__(self, llm_provider):
+        self.llm_provider = llm_provider
+        self.__read_prompts()
+        self.use_context_enabled = False
+        self.use_improvements_enabled = True
+        self.reset_context()
+
+    def __read_prompts(self):
+
+        sys_prompt_file_path = os.path.join(BASE_DIR, 'sys_prompt.txt')
+        with open(sys_prompt_file_path, 'r', encoding='UTF-8') as sys_prompt_file:
+            self.sys_prompt = sys_prompt_file.read()
+        
+        translate_sys_prompt_file_path = os.path.join(BASE_DIR, 'translate_sys_prompt.txt')
+        with open(translate_sys_prompt_file_path, 'r', encoding='UTF-8') as translate_sys_prompt_file:
+            self.translate_sys_prompt = translate_sys_prompt_file.read()
+
+    def use_context(self, enable):
+        self.use_context_enabled = enable
+        if not self.use_context_enabled:
+            self.reset_context()
+
+    def use_improvement(self, enable):
+        self.use_improvements_enabled = enable
+
+    def reprompt(self, text):
+        if not self.use_context_enabled:
+            self.reset_context()
+        self.content.append({ 'role': 'user', 'content': text, })
+        
+        return self.llm_provider.call_llm(self.content)
+
+    def reset_context(self):
+        self.content = []
+        # append system prompt
+        if self.use_improvements_enabled:
+            self.content.append({ 'role': 'system', 'content': self.sys_prompt, })
+        else:
+            self.content.append({ 'role': 'system', 'content': self.translate_sys_prompt, })
 
 class RemprompterScript(scripts.Script):
     def __init__(self) -> None:
         super().__init__()
 
-        self.reprompter_ollama_host = shared.opts.data.get("reprompter_ollama_host", "")
-        self.reprompter_ollama_model = shared.opts.data.get("reprompter_ollama_model", "")
-        self.reprompter_ollama_key = shared.opts.data.get("reprompter_ollama_key", "")
-        self.reprompter_ollama_use_history = shared.opts.data.get("reprompter_ollama_use_history", True)
-        self.reprompter_use_positive_improvements = shared.opts.data.get("reprompter_use_positive_improvements", False)
+        reprompter_openai_host = shared.opts.data.get("reprompter_openai_host", "")
+        self.reprompter_openai_model = shared.opts.data.get("reprompter_openai_model", "")
+        reprompter_openai_key = shared.opts.data.get("reprompter_openai_key", "")
+
+        llm_provider = LLMOpenAIProvider(reprompter_openai_host, self.reprompter_openai_model, reprompter_openai_key)
+        self.prompt = PromptBuilder(llm_provider)        
         
     def title(self):
         return REPROMPTER
@@ -34,60 +81,48 @@ class RemprompterScript(scripts.Script):
         return scripts.AlwaysVisible
     
     def make_reprompt(self, text):
-        print("[{}][{}] Process text: {}".format(REPROMPTER, self.reprompter_ollama_model, text))
+        print("[{}][{}] Process text: {}".format(REPROMPTER, self.reprompter_openai_model, text))
         try:
-            client = OpenAI(
-                base_url = self.reprompter_ollama_host,
-                api_key=self.reprompter_ollama_key,
-            )
 
-            query_sys_prompt = sys_prompt if self.reprompter_use_positive_improvements else translate_sys_prompt
-
-            request_content = [
-            {
-                'role': 'system',
-                'content': query_sys_prompt,
-            },
-            {
-                'role': 'user',
-                'content': text,
-            }]
-
-            response = client.chat.completions.create(model=self.reprompter_ollama_model, messages=request_content)
-            return response.choices[0].message.content
+            result = self.prompt.reprompt(text)
+            return result
         
         except Exception as e:
-            print("[{}] Error call ollama api: {}".format(REPROMPTER, repr(e)))
+            print("[{}] Error call openai api: {}".format(REPROMPTER, repr(e)))
         return text    
     
+    def __update_use_context(self, checkbox):
+        self.prompt.use_context(checkbox)
+
+    def __update_use_improvement(self, checkbox):
+        self.prompt.use_improvement(checkbox)
+
     def ui(self, is_img2img):
         with gr.Group():
             with gr.Accordion(REPROMPTER, open=False):
                 text_to_reprompt = gr.Textbox(label="Prompt description, any language")
+                checkbox_use_context =  gr.Checkbox(label="Use context")
+                checkbox_use_improvement =  gr.Checkbox(label="Use improvement", value=True,visible=True,)
                 send_text_button = gr.Button(value='Reprompt', variant='primary')
         
         with contextlib.suppress(AttributeError):  # Ignore the error if the attribute is not present
+            checkbox_use_context.change(fn=self.__update_use_context, inputs=[checkbox_use_context])
+            checkbox_use_improvement.change(fn=self.__update_use_improvement, inputs=[checkbox_use_improvement])
             if is_img2img:
                 send_text_button.click(fn=self.make_reprompt, inputs=[text_to_reprompt], outputs=[self.img2img])
             else:
                 send_text_button.click(fn=self.make_reprompt, inputs=[text_to_reprompt], outputs=[self.text2img])                
 
-        return [text_to_reprompt, send_text_button]
+        return [text_to_reprompt, checkbox_use_context, send_text_button]
 
     def on_ui_settings():
         section = ("reprompter", REPROMPTER)
-        shared.opts.add_option("reprompter_ollama_host", shared.OptionInfo(
-            "", "Ollama url", section=section).needs_reload_ui())
-        shared.opts.add_option("reprompter_ollama_model", shared.OptionInfo(
-            "", "Ollama Model", section=section).needs_reload_ui())
-        shared.opts.add_option("reprompter_ollama_key", shared.OptionInfo(
-            "", "Ollama API Key", section=section).needs_reload_ui())
-        shared.opts.add_option("reprompter_ollama_use_history", shared.OptionInfo(
-            False, "Use context to edit a prompt on the fly",
-            gr.Checkbox, {"interactive": True}, section=section).needs_reload_ui())
-        shared.opts.add_option("reprompter_use_positive_improvements", shared.OptionInfo(
-            True, "Utilize positive improvements",
-            gr.Checkbox, {"interactive": True}, section=section).needs_reload_ui())
+        shared.opts.add_option("reprompter_openai_host", shared.OptionInfo(
+            "", "OpenAI API Host", section=section).needs_reload_ui())
+        shared.opts.add_option("reprompter_openai_model", shared.OptionInfo(
+            "", "OpenAI API Model", section=section).needs_reload_ui())
+        shared.opts.add_option("reprompter_openai_key", shared.OptionInfo(
+            "", "OpenAI API Key", section=section).needs_reload_ui())
 
     script_callbacks.on_ui_settings(on_ui_settings)   
     

81e7d4b5baf38f1e65ead3627c6dbf4a7ffc0ebf
ogoun <ogoun.er@yandex.ru>
2025-01-28 21:43:42 +0300

    Update reprompter.py
    Fix using history in LLM context.
    Fix default selection in UI.
    Refactoring.

--- scripts/reprompter.py
+++ scripts/reprompter.py
@@ -9,19 +9,31 @@ from modules.shared import opts, state
 REPROMPTER = "Remprompter"
 BASE_DIR = scripts.basedir()
 
-# OpenAPI connector
 class LLMOpenAIProvider():
+    """A `LLMOpenAIProvider` is OpenAI API connector."""
+
     def __init__(self, host, model, key):
         self.host = host
         self.model = model
         self.key = key
 
     def call_llm(self, content):
-        client = OpenAI( base_url = self.host, api_key=self.key, )
-        response = client.chat.completions.create(model=self.model, messages=content)
-        return response.choices[0].message.content
+        """"Call LLM model."""
+
+        try:
+
+            client = OpenAI( base_url = self.host, api_key=self.key, )
+            response = client.chat.completions.create(model=self.model, messages=content)
+            return response.choices[0].message.content
+
+        except Exception as e:
+
+            print("[{}] Error call openai api: {}".format(REPROMPTER, repr(e)))
+
+        return None    
 
 class PromptBuilder():
+    """A `PromptBuilder` Creates a promt from user input text in any language."""
 
     def __init__(self, llm_provider):
         self.llm_provider = llm_provider
@@ -31,7 +43,7 @@ class PromptBuilder():
         self.reset_context()
 
     def __read_prompts(self):
-
+        """Read system prompts from files."""
         sys_prompt_file_path = os.path.join(BASE_DIR, 'sys_prompt.txt')
         with open(sys_prompt_file_path, 'r', encoding='UTF-8') as sys_prompt_file:
             self.sys_prompt = sys_prompt_file.read()
@@ -41,21 +53,37 @@ class PromptBuilder():
             self.translate_sys_prompt = translate_sys_prompt_file.read()
 
     def use_context(self, enable):
+        """Enables or disables the use of context for LLMs."""
+
         self.use_context_enabled = enable
         if not self.use_context_enabled:
             self.reset_context()
 
     def use_improvement(self, enable):
+        """Enables or disables the reworking and enhancement of promt text."""
+
         self.use_improvements_enabled = enable
 
     def reprompt(self, text):
+        """Creating a promt. If the service is unavailable from the LLM, the input text will be returned."""
+
         if not self.use_context_enabled:
             self.reset_context()
+
         self.content.append({ 'role': 'user', 'content': text, })
-        
-        return self.llm_provider.call_llm(self.content)
+
+        responce = self.llm_provider.call_llm(self.content)
+
+        if responce is not None:
+            if self.use_context_enabled:
+                self.content.append({ 'role': 'assistant', 'content': responce, })
+            return responce
+
+        return text
 
     def reset_context(self):
+        """Resetting the LLM context."""
+
         self.content = []
         # append system prompt
         if self.use_improvements_enabled:
@@ -64,6 +92,8 @@ class PromptBuilder():
             self.content.append({ 'role': 'system', 'content': self.translate_sys_prompt, })
 
 class RemprompterScript(scripts.Script):
+    """A `RemprompterScript` main extension script for WebUI."""
+
     def __init__(self) -> None:
         super().__init__()
 
@@ -73,33 +103,9 @@ class RemprompterScript(scripts.Script):
 
         llm_provider = LLMOpenAIProvider(reprompter_openai_host, self.reprompter_openai_model, reprompter_openai_key)
         self.prompt = PromptBuilder(llm_provider)        
-        
-    def title(self):
-        return REPROMPTER
-
-    def show(self, is_img2img):
-        return scripts.AlwaysVisible
-    
-    def make_reprompt(self, text):
-        print("[{}][{}] Process text: {}".format(REPROMPTER, self.reprompter_openai_model, text))
-        try:
-
-            result = self.prompt.reprompt(text)
-            return result
-        
-        except Exception as e:
-            print("[{}] Error call openai api: {}".format(REPROMPTER, repr(e)))
-        return text    
-    
-    def __update_use_context(self, enable):
-        # print('[{}] Use context: {}'.format(REPROMPTER, enable))
-        self.prompt.use_context(enable)
-
-    def __update_use_improvement(self, enable):
-        # print('[{}] Use improvement: {}'.format(REPROMPTER, enable))
-        self.prompt.use_improvement(enable)
 
     def __update_parameters(self, checkboxGroup):
+        """Updates the flags responsible for using context for LLM and for text enhancement."""
         use_context = False
         use_improvement = False
         for p in checkboxGroup:
@@ -107,15 +113,25 @@ class RemprompterScript(scripts.Script):
                 use_context = True
             if "improvement" in p:
                 use_improvement = True
-        self.__update_use_context(use_context)
-        self.__update_use_improvement(use_improvement)
+        self.prompt.use_context(use_context)
+        self.prompt.use_improvement(use_improvement)
+
+    def title(self): return REPROMPTER
+
+    def show(self, is_img2img): return scripts.AlwaysVisible
+    
+    def make_reprompt(self, text): 
+        print("[{}][{}] Process text: {}".format(REPROMPTER, self.reprompter_openai_model, text))
+        return self.prompt.reprompt(text)
 
     def ui(self, is_img2img):
+
+        choices = ["Use context", "Use improvement"]
         with gr.Group():
             with gr.Accordion(REPROMPTER, open=False):                
                 text_to_reprompt = gr.Textbox(label="Prompt description, any language")
                 gr.HTML("<br style='margin-top: 10px;'>")
-                parameters = gr.CheckboxGroup(["Use context", "Use improvement"], label="Query parameters")
+                parameters = gr.CheckboxGroup(choices=choices, label="Query parameters", interactive=True, value=[choices[1]])
                 gr.HTML("<br style='margin-top: 20px;'>")
                 send_text_button = gr.Button(value='Reprompt', variant='primary')
        
@@ -138,7 +154,7 @@ class RemprompterScript(scripts.Script):
         shared.opts.add_option("reprompter_openai_key", shared.OptionInfo(
             "", "OpenAI API Key", section=section).needs_reload_ui())
 
-    script_callbacks.on_ui_settings(on_ui_settings)   
+    script_callbacks.on_ui_settings(on_ui_settings)
     
     def after_component(self, component, **kwargs):
         if kwargs.get("elem_id") == "txt2img_prompt":
